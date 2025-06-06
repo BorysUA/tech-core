0662379d0137b0a25d6cb358966c25c977e13dfa
BorysUA <94442769+BorysUA@users.noreply.github.com>
2025-06-06 10:45:04 +0300

    Rename VectorExtensions file and class

--- Assets/_Project/CodeBase/Extensions/VectorExtensions.cs
+++ Assets/_Project/CodeBase/Extensions/VectorExtensions.cs
@@ -2,7 +2,7 @@
 
 namespace _Project.CodeBase.Extensions
 {
-  public static class VectorExtenstions
+  public static class VectorExtensions
   {
     public static Vector3 ToXZ(this Vector3 vector) => 
       new(vector.x, 0, vector.z);
